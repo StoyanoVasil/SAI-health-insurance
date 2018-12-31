69becbd5419ad3214b3d54098b6eba2e854f35e5
i333656 <vasil.stoyanov@feg.bg>
2018-12-31 17:55:26 +0100

    Add serializers from hospital and insurance-clinet in broker

---
+++ broker/src/main/java/broker/gateway/HospitalCostsSerializer.java
@@ -0,0 +1,66 @@
+package broker.gateway;
+
+import broker.model.hospital.HospitalCostsReply;
+import broker.model.hospital.HospitalCostsRequest;
+import com.google.gson.Gson;
+
+/**
+ * A class that is responsible for serializing to a JSON sting and
+ * deserializing  from a JSON string HospitalCostsRequest and HospitalCostsReply objects
+ */
+public class HospitalCostsSerializer {
+
+    // Declare Gson object that does the (de)serializing
+    private Gson serializer;
+
+    /**
+     * Constructor that initializes the Gson object
+     */
+    public HospitalCostsSerializer() {
+        this.serializer = new Gson();
+    }
+
+    /**
+     * Serializes HospitalCostsRequest to a JSON string by using the Gson object
+     * then returns the serialized string
+     *
+     * @param hospitalCostsRequest HospitalCostsRequest object to be serialized
+     * @return String hospitalCostsRequest serialized to a JSON string
+     */
+    public String serializeHospitalCostsRequest(HospitalCostsRequest hospitalCostsRequest) {
+        return this.serializer.toJson(hospitalCostsRequest);
+    }
+
+    /**
+     * Serializes HospitalCostsReply to a JSON string by using the Gson object
+     * then returns the serialized string
+     *
+     * @param hospitalCostsReply HospitalCostsRequest object to be serialized
+     * @return String hospitalCostsReply serialized to a JSON string
+     */
+    public String serializeHospitalCostsReply(HospitalCostsReply hospitalCostsReply) {
+        return this.serializer.toJson(hospitalCostsReply);
+    }
+
+    /**
+     * Deserializes a JSON string to a HospitalCostsRequest object by using the Gson object,
+     * then returns the deserialized object
+     *
+     * @param json JSON string to be deserialized
+     * @return HospitalCostsRequest the deserialized object
+     */
+    public HospitalCostsRequest deserializeHospitalCostsRequestJSON(String json) {
+        return this.serializer.fromJson(json, HospitalCostsRequest.class);
+    }
+
+    /**
+     * Deserializes a JSON string to a HospitalCostsReply object by using the Gson object,
+     * then returns the deserialized object
+     *
+     * @param json JSON string to be deserialized
+     * @return HospitalCostsReply the deserialized object
+     */
+    public HospitalCostsReply deserializeHospitalCostsReplyJSON(String json) {
+        return this.serializer.fromJson(json, HospitalCostsReply.class);
+    }
+}
---
+++ broker/src/main/java/broker/gateway/TreatmentCostsSerializer.java
@@ -0,0 +1,66 @@
+package broker.gateway;
+
+import broker.model.client.TreatmentCostsReply;
+import broker.model.client.TreatmentCostsRequest;
+import com.google.gson.Gson;
+
+/**
+ * A class that is responsible for serializing to a JSON sting and
+ * deserializing  from a JSON string TreatmentCostsRequest and TreatmentCostsReply objects
+ */
+public class TreatmentCostsSerializer {
+
+    // Declare Gson object that does the (de)serializing
+    private Gson serializer;
+
+    /**
+     * Constructor that initializes the Gson object
+     */
+    public TreatmentCostsSerializer() {
+        this.serializer = new Gson();
+    }
+
+    /**
+     * Serializes TreatmentCostRequest to a JSON string by using the Gson object
+     * then returns the serialized string
+     *
+     * @param treatmentCostsRequest TreatmentCostRequest object to be serialized
+     * @return String treatmentCostsRequest serialized to a JSON string
+     */
+    public String serializeTreatmentCostsRequest(TreatmentCostsRequest treatmentCostsRequest) {
+        return this.serializer.toJson(treatmentCostsRequest);
+    }
+
+    /**
+     * Serializes TreatmentCostReply to a JSON string by using the Gson object
+     * then returns the serialized string
+     *
+     * @param treatmentCostsReply TreatmentCostRequest object to be serialized
+     * @return String treatmentCostsReply serialized to a JSON string
+     */
+    public String serializeTreatmentCostsReply(TreatmentCostsReply treatmentCostsReply) {
+        return this.serializer.toJson(treatmentCostsReply);
+    }
+
+    /**
+     * Deserializes a JSON string to a TreatmentCostRequest object by using the Gson object,
+     * then returns the deserialized object
+     *
+     * @param json JSON string to be deserialized
+     * @return TreatmentCostRequest the deserialized object
+     */
+    public TreatmentCostsRequest deserializeTreatmentCostsRequestJSON(String json) {
+        return this.serializer.fromJson(json, TreatmentCostsRequest.class);
+    }
+
+    /**
+     * Deserializes a JSON string to a TreatmentCostReply object by using the Gson object,
+     * then returns the deserialized object
+     *
+     * @param json JSON string to be deserialized
+     * @return TreatmentCostReply the deserialized object
+     */
+    public TreatmentCostsReply deserializeTreatmentCostsReplyJSON(String json) {
+        return this.serializer.fromJson(json, TreatmentCostsReply.class);
+    }
+}
