a0e399fe006de2843f2af806b216e3722d11279b
i333656 <vasil.stoyanov@feg.bg>
2018-12-31 18:41:56 +0100

    Add BrokerListLine to broker

---
+++ broker/src/main/java/broker/application/BrokerListLine.java
@@ -0,0 +1,38 @@
+package broker.application;
+
+
+import broker.model.hospital.HospitalCostsReply;
+import broker.model.hospital.HospitalCostsRequest;
+
+public class BrokerListLine {
+
+	private HospitalCostsRequest request;
+	private HospitalCostsReply reply;
+
+	public BrokerListLine(HospitalCostsRequest request, HospitalCostsReply reply) {
+            this.reply = reply;
+            this.request = request;
+	}	
+	
+	public HospitalCostsRequest getRequest() {
+		return request;
+	}
+	
+	private void setRequest(HospitalCostsRequest request) {
+		this.request = request;
+	}
+	
+	public HospitalCostsReply getReply() {
+		return reply;
+	}
+	
+	public void setReply(HospitalCostsReply reply) {
+		this.reply = reply;
+	}
+	
+	@Override
+	public String toString() {
+	   return request.toString() + "  --->  " + ((reply!=null)?reply.toString():"waiting...");
+	}
+	
+}
